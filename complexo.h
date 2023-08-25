87b52fad33051d699db2f1a042fee2edb35af534
Luiz Henrique Botega Beraldi <139796218+LurdesBB@users.noreply.github.com>
2023-08-25 18:55:47 -0300

    complexo.h

--- complexo.h
+++ complexo.h
@@ -3,10 +3,8 @@ typedef struct{
     float imaginario;
 }complexo;
 
-int i=0;
-
 complexo complexo_novo(float real, float imaginario);
 complexo complexo_soma(complexo a, complexo b);
 complexo complexo_le();
 complexo complexo_imprime(complexo a);
-complexo complexo_verifica(complexo a,int *i);
+complexo complexo_verifica(complexo a);
