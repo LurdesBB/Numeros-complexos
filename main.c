6ab33a73a08f55dacc4d7f92ced542309ffc97bc
Luiz Henrique Botega Beraldi <139796218+LurdesBB@users.noreply.github.com>
2023-08-25 18:26:36 -0300

    main.c

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include "complexo.h"
 
 int main(){
+    int i=0;
     complexo a, b, c;
     a = complexo_le();
     b = complexo_le();
     c = complexo_soma(a,b);
     complexo_imprime(c);
-    complexo_verifica(a);
-    complexo_verifica(b);
+    complexo_verifica(a,&i);
+    complexo_verifica(b,&i);
+    complexo_verifica(c,&i);
     return 0;
 }
